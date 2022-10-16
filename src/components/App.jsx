957ee41c73d3902ae23e3b819118061c3cc45e8f
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-10-16 22:59:45 +0530

    Add Notes Addition and Deletion feature + Finishing Touches

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Note from "./Note";
 import Footer from "./Footer";
-import notes from "../notes";
+import CreateArea from "./CreateArea";
+// import notes from "../notes";
 
 function App() {
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevNotes) => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes((prevNotes) =>
+      prevNotes.filter((noteItem, index) => index !== id)
+    );
+  }
+
   return (
     <div>
       <Header />
-
-      {notes.map((note) => (
-        <Note key={note.key} title={note.title} content={note.content} />
-      ))}
-
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
       <Footer />
+
+      {/* {notes.map((note) => (
+        <Note key={note.key} title={note.title} content={note.content} />
+      ))} */}
     </div>
   );
 }
