bdb8a5f16fdd187f82f3ff3ab7621534abce0f03
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-10-16 17:14:10 +0530

    Add Notes display mechanism

--- src/index.js
+++ src/index.js
@@ -3,5 +3,3 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//HINT: You will need to study the classes in teh styles.css file to appy styling.
